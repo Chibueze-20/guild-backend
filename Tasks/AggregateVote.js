2c87ee9806bb7aee19e8a6b8047f095a26cd2551
Okedele <okedelep@gmail.com>
2021-09-13 22:20:44 +0100

    used UTC time for cron job and added check for aggregating votes

--- Tasks/AggregateVote.js
+++ Tasks/AggregateVote.js
@@ -14,27 +14,36 @@ const AggregateVote = cron.schedule("0 0 * * 1", async () => {
   let votes = await AnimeVote.find({
     createdAt: { $gte: last_week, $lt: new Date() },
   });
-  let total_votes = [];
-  let season = votes[0].season;
-  let year = votes[0].year;
-  for (const vote of votes) {
-    vote.votes.forEach((element) => {
-      total_votes.push(element);
+  if (votes.length >= 1) {
+    let total_votes = [];
+    let season = votes[0].season;
+    let year = votes[0].year;
+    for (const vote of votes) {
+      vote.votes.forEach((element) => {
+        total_votes.push(element);
+      });
+    }
+    let weekly_votes = [];
+    total_votes.reduce(function (res, value) {
+      if (!res[value.anime]) {
+        res[value.anime] = { anime: value.anime, vote: 0 };
+        weekly_votes.push(res[value.anime]);
+      }
+      res[value.anime].vote += value.vote;
+      return res;
+    }, {});
+    const week_check = await WeeklyVote.find({ season: season, year: year });
+    const week = week_check ? week_check.length + 1 : 1;
+    await WeeklyVote.create({
+      votes: weekly_votes,
+      week: week,
+      season: season,
+      year: year,
     });
   }
-  let weekly_votes = [];
-  total_votes.reduce(function (res, value) {
-    if (!res[value.anime]) {
-      res[value.anime] = { anime: value.anime, vote: 0 };
-      weekly_votes.push(res[value.anime]);
-    }
-    res[value.anime].vote += value.vote;
-    return res;
-  }, {});
-  const week_check = await WeeklyVote.find({ season: season, year: year });
-  const week = week_check ? week_check.length + 1 : 1;
-  await WeeklyVote.create({ votes: weekly_votes, week: week, season: season, year: year });
   console.log("Task ended");
+}, {
+  timezone: "UTC"
 });
 
 module.exports = AggregateVote;
